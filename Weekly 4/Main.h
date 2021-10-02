bff4eac01a60a07a6ffcead8bb13e31ac3c88797
Staal22 <staalbr@gmail.com>
2021-10-02 17:43:36 +0200

    header

--- Weekly 4/Main.h
+++ Weekly 4/Main.h
@@ -1,7 +1,8 @@
-#pragma once
 #include <iostream>
 #include <conio.h>
 
+#ifndef MAIN
+#define MAIN
 
 void clearCin();
 void task1();
@@ -13,3 +14,5 @@ void task4();
 From learncpp.com:
 If a header file is paired with a code file(e.g.add.h with add.cpp), they should both have the same base name(add)
 */
+
+#endif !MAIN
